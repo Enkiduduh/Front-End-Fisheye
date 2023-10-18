e56544b5707aefbef0e5479151d5ee84527ab4e0
Ilyes Soumar Djouma <isoumardjouma@reseau.free.fr>
2023-10-18 21:26:07 +0200

    fetch json part ongoing

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,26 +1,13 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
+        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        let  photographers= [{}];
+        const url = 'data/photographers.json';
+        fetch(url)
+        .then(res => res.json())
+        .then(data => data.photographers);
+
+
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
             photographers: [...photographers, ...photographers, ...photographers]})
@@ -41,6 +28,5 @@
         const { photographers } = await getPhotographers();
         displayData(photographers);
     }
-    
+
     init();
-    
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,34 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+// const url = 'data/photographers.json';
+//         fetch(url)
+//          .then((resp) => resp.json())
+//          console.log(resp)
+//          .then(function(data){
+//             let photographers = data.results;
+//          });
+// function getPhotographers() {
+
+const output1 = document.getElementById("output1");
+const output2 = document.getElementById("output2");
+const output3 = document.getElementById("output3");
+const output4 = document.getElementById("output4");
+const img = document.getElementById("img-output");
+
+fetch('data/photographers.json')
+  .then(res => res.json())
+  .then(data => {
+    output1.textContent =`Infos du photogrape: ${data.photographers[0].name}`
+    output2.textContent =`Id: ${data.photographers[0].id}`;
+    output3.textContent =`Ville: ${data.photographers[0].city} `;
+    output4.textContent =`Tagline ${data.photographers[0].tagline}`;
+    console.log( data.photographers[0].portrait)
+    img.src = data.photographers[0].portrait;
+    img.width = 100;
+  });
+
+
+
+
+// }
+// getPhotographers();
